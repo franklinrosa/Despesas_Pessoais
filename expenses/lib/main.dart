977c06e2aef6afb73e91ce0d9a7b3345b8a6ab24
Franklin Rosa <franklingarciadarosa@gmail.com>
2024-05-03 20:44:41 -0300

    refazendo app

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,173 +1,28 @@
-import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter/services.dart';
-import 'dart:math';
-//import 'components/transaction_form.dart';
-import 'components/transaction_list.dart';
-import 'components/chart.dart';
-import 'models/transaction.dart';
 
-main() => runApp(ExpensesApp());
+main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  ExpensesApp({Key? key}) : super(key: key);
-  final ThemeData tema = ThemeData();
+  const ExpensesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //Esta função impede o app funcionar em modo paisagem
-    //SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
-      theme: tema.copyWith(
-        colorScheme: tema.colorScheme.copyWith(
-          //  onPrimary: Colors.white,
-          //  onSecondary: Colors.white,
-          primary: const Color.fromARGB(255, 47, 5, 97),
-          secondary: const Color.fromARGB(255, 25, 10, 47),
-        ),
-        textTheme: tema.textTheme.copyWith(
-          titleLarge: const TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+    return const MaterialApp(home: MyHomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _transactions = [];
-  bool _showChart = false;
-
-  List<Transaction> get _recentTransactions {
-    return _transactions.where((tr) {
-      return tr.date.isAfter(DateTime.now().subtract(
-        const Duration(days: 7),
-      ));
-    }).toList();
-  }
-
-  _addTransaction(String title, double value, DateTime date) {
-    final newTransaction = Transaction(
-      id: Random().nextDouble().toString(),
-      title: title,
-      value: value,
-      date: date,
-    );
-
-    setState(() {
-      _transactions.add(newTransaction);
-    });
-
-    Navigator.of(context).pop();
-  }
-
-  _removeTransaction(String id) {
-    setState(() {
-      _transactions.removeWhere(
-        (tr) => tr.id == id,
-      );
-    });
-  }
-
-  _openTransactionFormModal(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (_) {
-        return TransactionForm(_addTransaction);
-      },
-    );
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
-
-    final appBar = AppBar(
-      title: const Text(
-        'DESPESAS PESSOAIS',
-      ),
-      actions: [
-        if (isLandscape)
-          IconButton(
-            icon: Icon(_showChart ? Icons.list : Icons.show_chart),
-            onPressed: () {
-              setState(() {
-                _showChart = !_showChart;
-              });
-            },
-          ),
-        IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: () => _openTransactionFormModal(context),
-        ),
-      ],
-    );
-    final availabelHeigth = mediaQuery.size.height -
-        appBar.preferredSize.height -
-        mediaQuery.padding.top;
-
     return Scaffold(
-      appBar: appBar,
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            /*if (isLandscape)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Exibir Gráfico'),
-                  Switch(
-                    value: _showChart,
-                    onChanged: (value) {
-                      setState(() {
-                        _showChart = value;
-                      });
-                    },
-                  ),
-                ],
-              ), */
-            if (_showChart || !isLandscape)
-              SizedBox(
-                height: availabelHeigth * (isLandscape ? 0.7 : 0.25),
-                child: Chart(_recentTransactions),
-              ),
-            if (!_showChart || !isLandscape)
-              SizedBox(
-                height: availabelHeigth * 0.75,
-                child: TransactionList(_transactions, _removeTransaction),
-              ),
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => _openTransactionFormModal(context),
+      body: const Center(
+        child: Text("Versão Inicial"),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
