cee27177ebcc28d96e8ddf0dec818275054d8069
Franklin Rosa <franklingarciadarosa@gmail.com>
2024-05-03 22:36:48 -0300

    atualizacao

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,28 +1,112 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-main() => runApp(const ExpensesApp());
+void main() {
+  runApp(
+    const MaterialApp(
+      home: ExpensesApp(),
+    ),
+  );
+}
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final _transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tenis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Despesas Pessoais'),
-      ),
-      body: const Center(
-        child: Text("Versão Inicial"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Despesas Pessoais'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: const Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Gráfico'),
+              ),
+            ),
+            Column(
+              //função map para mostrar os elementos da lista _transaction
+              children: _transaction.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        //envolvido em Container para dar efeitos visuais
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tr.title,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            //data formatada com intl
+                            DateFormat('d MMM y').format(tr.date),
+                            style: const TextStyle(
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
   }
 }
